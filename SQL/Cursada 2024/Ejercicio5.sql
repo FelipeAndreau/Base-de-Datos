53ba0c01335b42620feba5dc03fee5f52032c239
FelipeAndreau <felipe.andreau@alu.frlp.utn.edu.ar>
2025-04-28 15:41:18 -0300

    Create Ejercicio5.sql

---
+++ SQL/Cursada 2024/Ejercicio5.sql
@@ -0,0 +1,45 @@
+A. Juan Barnetche, Miguel Valcaneras y Analia Zamora, trabajan y están registrados en la Empresa Belpe.
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Juan Barnetche','JB','JuanBarnetche.com','Rivadavia','7418-3690')
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Miguel Valcaneras','MV','MiguelValcaneras.com','Rivadavia','7418-2587')
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Analia Zamora','AN','AnaliaZamora.com','Rivadavia','7632-2587')
+insert into UsuarioxEmpresa(idUsuario,idEmpresa)
+values
+('8A566EBB-D19B-4039-B5A5-26AF7BA4B54A','5'),('7BF4ED57-AE4B-4B13-8A4C-5B30DEC68F1A','5',
+('C0098761-5785-4F90-8483-99CFCD99BD55', '5')
+B. Maximiliano Moreno y Tamara Castro, trabajan en CGO.
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Maximiliano Moreno','MM','MaximilianoMoreno.com','Rivadavia','74148-515')
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Tamara Castro','TC','TamaraCastro.com','Rivadavia','41148-515')
+insert into UsuarioxEmpresa(idUsuario, idEmpresa) values ( 'ED2BCCFB -2F14 -4FA8 -B84A
+-31E7E165C71A','4’),('03AC154A-AAE3-41D1-AC9E-B77C8EE8A7A4','4')
+C. Miguel Suarez es el único usuario registrado en Az Motor.
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Miguel Suarez','MS','MiguelSuarez.com','Rivadavia','741-515')
+insert into UsuarioxEmpresa(idUsuario, idEmpresa)
+values ('B601D7BA-1147-4109-9D9F-DA988C287245','7')
+D. Enrique Moreno, Lionel Galo y Esperanza Mendez, trabajan en Goicoechea. Pero, ésta última, Esperanza, también
+hace trabajos y está registrada en Vaer S. A.
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Enrique Moreno','EM','EnriqueMoreno.com','Rivadavia','74133-515')
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Lionel Galo','LG','LionelGalo.com','Rivadavia','7121-515')
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Esperanza Mendez','EM','EsperanzaMendez.com','Rivadavia','8693-515')
+insert into UsuarioxEmpresa(idUsuario, idEmpresa)
+values ('5848A006-9A65-4CA0-B26E-BDB411D93082','3'),
+('106EBB47-610A-416D-A147-9AAF2B930DFA','3'),
+('F7CD5750-7956-4C5D-859D-5FF9B9E20B63','3')
+insert into UsuarioxEmpresa(idUsuario, idEmpresa)
+values ('F7CD5750-7956-4C5D-859D-5FF9B9E20B63','2')
+E. Albocar, tiene dos usuarios. Uno es Maximiliano Gonzalez, y el otro Martín Perez.
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Maximiliano Gonzalez','MG','MaximilianoGonzalez.com','Rivadavia','893-515')
+insert into Usuario (AyN, Contrasena, Email, Domicilio, Telefono)
+values ('Martín Perez','MP','MartínPerez.com','Rivadavia','8669-515')
+insert into UsuarioxEmpresa(idUsuario, idEmpresa)
+values ('CA0937E4-D9EF-4728-963D-9B8A22D46E24','1'),
+('CD643F36-3795-44F9-871B-FD1AC94AE413','1')
