929f866533ea6b545c0ec0d5af325bebd8936a53
FelipeAndreau <felipe.andreau@alu.frlp.utn.edu.ar>
2025-04-28 09:18:22 -0300

    Update Ejercicio3.sql

--- SQL/Cursada 2024/Ejercicio3.sql
+++ SQL/Cursada 2024/Ejercicio3.sql
@@ -1,1 +1,18 @@
+-- A. Eliminar de la tabla usuario el campo Usuario.
+
+ALTER TABLE Usuario DROP COLUMN Usuario
+
+-- B. Agregar un nuevo campo que sea email (varchar(70))
+
+ALTER TABLE Usuario ADD Email VARCHAR(70)
+  
+-- C. Agregar los campos Domicilio (varchar(150)) y Telefono (varcvhar(30)) en la tabla Usuario.
+
+  ALTER TABLE Usuario ADD Domicilio varchar(150)
+  ALTER TABLE Usuario ADD Telefono varchar(30)
+
+-- D. Luego, agregar los mismos campos a la tabla Empresa.
+
+ALTER TABLE Empresa ADD Domicilio varchar(150)
+ALTER TABLE Empresa ADD Telefono varchar(30)
 
